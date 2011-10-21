dc69d621732a32e20f39bbb979804fe178cee6bd
Alex MacCaw <maccman@gmail.com>
2011-10-21 09:56:27 +0100

    some view refactoring

--- lib/generators/spine/view/view_generator.rb
+++ lib/generators/spine/view/view_generator.rb
@@ -6,12 +6,6 @@ module Spine
       source_root File.expand_path("../templates", __FILE__)
       desc "Generate a Spine views grouped by the name of the NAME argument"
 
-      argument :actions, :desc => 'The names of the views', :type => :array, :banner => 'index show edit whatever'
-
-      def create_views_directory
-        empty_directory "app/assets/javascripts/views/#{file_name}"
-      end
-
       # rails g spine:view Users index edit show
       def create_views
         view_format = if defined?(::Eco)
@@ -22,9 +16,7 @@ module Spine
           'ejs'
         end
 
-        actions.each do |view_name|
-          template "view.#{view_format}.erb", "app/assets/javascripts/views/#{file_name}/#{view_name}.jst.#{view_format}", :view_name => view_name
-        end
+        template "view.#{view_format}.erb", "app/assets/javascripts/views/#{file_name}.jst.#{view_format}"
       end
     end
   end
