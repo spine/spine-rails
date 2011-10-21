5c893019735ff19543a3b381ff60a4493239b9bc
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-21 15:05:33 +1100

    added simple controller generator

---
+++ lib/generators/spine/controller/controller_generator.rb
@@ -0,0 +1,15 @@
+require 'rails'
+
+module Spine
+  module Generators
+    class ControllerGenerator < ::Rails::Generators::NamedBase
+      source_root File.expand_path("../templates", __FILE__)
+      desc "Generate a Spine controller"
+
+      def create_controller
+        template "controller.coffee.erb", "app/assets/javascripts/controllers/#{file_name}.js.coffee"
+      end
+
+    end
+  end
+end
