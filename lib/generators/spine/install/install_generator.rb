2f93ec4d703900c81a6707d8dec72230d74ba882
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-21 14:02:43 +1100

    added install generator for Rails 3.1

--- lib/generators/spine/install/install_generator.rb
+++ lib/generators/spine/install/install_generator.rb
@@ -2,17 +2,46 @@ require 'rails'
 
 module Spine
   module Generators
-    class InstallGenerator < ::Rails::Generators::Base
+    if ::Rails.version < "3.1"
+      # Rails < 3.1
 
-      desc "This generator installs Spine #{Spine::Rails::SPINE_VERSION}"
+      class InstallGenerator < ::Rails::Generators::Base
 
-      source_root File.expand_path('../../../../../app/assets/javascripts', __FILE__)
+        desc "This generator installs Spine #{Spine::Rails::SPINE_VERSION}"
 
-      def copy_spine
-        say_status("copying", "Spine (#{Spine::Rails::SPINE_VERSION})", :green)
-        copy_file "spine.js", "public/javascripts/spine.js"
-        copy_file "spine.min.js", "public/javascripts/spine.min.js"
+        source_root File.expand_path('../../../../../app/assets/javascripts', __FILE__)
+
+        def copy_spine
+          say_status("copying", "Spine (#{Spine::Rails::SPINE_VERSION})", :green)
+          copy_file "spine.js", "public/javascripts/spine.js"
+          copy_file "spine.min.js", "public/javascripts/spine.min.js"
+        end
       end
-    end
+
+    else
+      # Rails >= 3.1
+
+      class InstallGenerator < Rails::Generators::Base
+        source_root File.expand_path("../templates", __FILE__)
+    
+        desc "This generator installs Spine #{Spine::Rails::SPINE_VERSION} as part of assets pipeline"
+            
+        def create_dir_layout
+          %W{models views controllers}.each do |dir|
+            empty_directory "app/assets/javascripts/#{dir}" 
+            create_file "app/assets/javascripts/#{dir}/.gitkeep"
+          end
+        end
+      
+        def create_app_file
+          copy_file "app.coffee", "app/assets/javascripts/app.js.coffee"
+        end
+       
+        def add_spine_app_to_application
+          append_file "app/assets/javascripts/application.js", "//= require app"
+        end
+      end
+
+    end # if Rails.version
   end
-end if ::Rails.version < "3.1"
+end
---
+++ lib/generators/spine/install/templates/app.coffee
@@ -0,0 +1,8 @@
+#= require_self
+#= require_tree ./models
+#= require_tree ./views
+#= require_tree ./controllers
+
+# Export the Models, Controllers into App object
+# TODO: Consider App.Models, App.Controllers, App.Views
+window.App = {}
