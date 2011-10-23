524285beacc38971f655553ae9a95fce02c836b9
Alex MacCaw <maccman@gmail.com>
2011-10-23 02:00:58 +0100

    add scaffold generator

---
+++ lib/generators/spine/scaffold/scaffold_generator.rb
@@ -0,0 +1,40 @@
+require "rails"
+require "spine/generators"
+
+module Spine
+  module Generators
+    class ScaffoldGenerator < Base
+      source_root File.expand_path("../templates", __FILE__)
+      desc "Generate a Spine scaffold with configured fields"
+
+      argument :fields, :desc => "List of model attributes", :type => :array, :banner => "field1 field2"
+      
+      def model_name
+        file_name.singularize
+      end
+      
+      def model_class
+        class_name.singularize
+      end
+      
+      def controller_name
+        file_name.pluralize
+      end
+      
+      def controller_class
+        class_name.pluralize
+      end
+      
+      def create_scaffold
+        raise("Eco required - please add to Gemfile") unless defined?(::Eco)
+        
+        generate "spine:model #{model_name} #{fields.join(" ")} --app #{app_name}"
+        template "controller.coffee.erb", "app/assets/javascripts/#{app_name}/controllers/#{controller_name}.js.coffee"
+        template "edit.jst.erb",  "app/assets/javascripts/#{app_name}/views/#{controller_name}/edit.jst.eco"
+        template "index.jst.erb", "app/assets/javascripts/#{app_name}/views/#{controller_name}/index.jst.eco"
+        template "new.jst.erb",   "app/assets/javascripts/#{app_name}/views/#{controller_name}/new.jst.eco"
+        template "show.jst.erb",  "app/assets/javascripts/#{app_name}/views/#{controller_name}/show.jst.eco"
+      end
+    end
+  end
+end
