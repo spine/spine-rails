4b1a74fda7ea3de844f9191441a97a0c6126957a
Alex MacCaw <maccman@gmail.com>
2011-10-23 07:38:42 +0100

    protect methods, so they are not called as generators (via @dnagir)

--- lib/spine/generators.rb
+++ lib/spine/generators.rb
@@ -3,17 +3,19 @@ module Spine
     class Base < ::Rails::Generators::NamedBase
       class_option :app, :type => :string, :default => "app", :desc => "app name"
       
-      def class_name
-        (class_path + [file_name]).map!{ |m| m.camelize }.join('')
-      end
+      protected
       
-      def app_name
-        options[:app]
-      end
+        def class_name
+          (class_path + [file_name]).map!{ |m| m.camelize }.join('')
+        end
       
-      def app_class
-        app_name.camelize
-      end
+        def app_name
+          options[:app]
+        end
+      
+        def app_class
+          app_name.camelize
+        end
     end
   end
 end
