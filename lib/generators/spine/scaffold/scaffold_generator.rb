fa8e95ca2705cf8c5bce15dad7ecbc14fb1bdad1
Alex MacCaw <maccman@gmail.com>
2011-10-23 07:40:39 +0100

    protect methods, so they are not called as generators

--- lib/generators/spine/scaffold/scaffold_generator.rb
+++ lib/generators/spine/scaffold/scaffold_generator.rb
@@ -9,24 +9,8 @@ module Spine
 
       argument :fields, :desc => "List of model attributes", :type => :array, :banner => "field1 field2"
       
-      def model_name
-        file_name.singularize
-      end
-      
-      def model_class
-        class_name.singularize
-      end
-      
-      def controller_name
-        file_name.pluralize
-      end
-      
-      def controller_class
-        class_name.pluralize
-      end
-      
       def create_scaffold
-        raise("Eco required - please add to Gemfile") unless defined?(::Eco)
+        raise("The 'eco' gem is required; add it to the Gemfile") unless defined?(::Eco)
         
         generate "spine:model #{model_name} #{fields.join(" ")} --app #{app_name}"
         template "controller.coffee.erb", "app/assets/javascripts/#{app_name}/controllers/#{controller_name}.js.coffee"
@@ -35,6 +19,24 @@ module Spine
         template "new.jst.erb",   "app/assets/javascripts/#{app_name}/views/#{controller_name}/new.jst.eco"
         template "show.jst.erb",  "app/assets/javascripts/#{app_name}/views/#{controller_name}/show.jst.eco"
       end
+      
+      protected
+        
+        def model_name
+          file_name.singularize
+        end
+
+        def model_class
+          class_name.singularize
+        end
+
+        def controller_name
+          file_name.pluralize
+        end
+
+        def controller_class
+          class_name.pluralize
+        end
     end
   end
 end
