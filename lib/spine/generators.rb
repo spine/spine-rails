7868fa3fc6295ad1b9020ea6dd68a793da25cf3f
Dmytrii Nagirniak <dnagir@gmail.com>
2011-10-23 17:01:51 +1100

    merged multiple view engines with jasmine specs generator

--- lib/spine/generators.rb
+++ lib/spine/generators.rb
@@ -3,6 +3,7 @@ module Spine
     class Base < ::Rails::Generators::NamedBase
       class_option :app, :type => :string, :default => "app", :desc => "app name"
       
+      # These are just helpers, and should not be executed. Thus must be protected.
       protected
 
       def class_name
@@ -27,6 +28,24 @@ module Spine
         end
       end
 
+      def object_name
+        file_name.camelize
+      end
+
+      def generate_jasmine_spec subject, path
+        return false unless defined?(::Jasmine)
+        variable_name = "#{subject.underscore}"
+
+        # TODO: Consider moving this into a template
+        create_file "spec/javascripts/#{app_name}/#{path}/#{variable_name}_spec.coffee", <<-SPEC
+describe "#{subject}", ->
+  #{variable_name} = null
+  beforeEach -> # init #{variable_name} here
+    
+  it "should be tested", ->
+    expect(#{variable_name}).toBeTruthy()
+SPEC
+      end
     end
   end
 end
