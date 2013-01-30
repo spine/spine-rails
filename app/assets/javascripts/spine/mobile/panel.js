6be44695b2713da97d8c7ccac1b47792fcbad409
Timo Uhlmann <timouhlmann@googlemail.com>
2013-01-30 20:11:56 +0100

    Added spine.mobile

---
+++ app/assets/javascripts/spine/mobile/panel.js
@@ -0,0 +1,1 @@
+<html><body>You are being <a href="https://raw.github.com/maccman/spine.mobile/master/lib/panel.js">redirected</a>.</body></html>
---
+++ app/assets/javascripts/spine/mobile/stage.js
@@ -0,0 +1,1 @@
+<html><body>You are being <a href="https://raw.github.com/maccman/spine.mobile/master/lib/stage.js">redirected</a>.</body></html>
