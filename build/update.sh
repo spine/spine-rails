9ec828d2246c4ce6a5059f200544321495c81fb9
Ben Haines-Bellamy <benhaines@me.com>
2013-04-17 03:23:43 +1200

    Use vendor path instead of app path

--- build/update.sh
+++ build/update.sh
@@ -1,9 +1,9 @@
-VERSION=v1.0.9
+TAG=v1.0.9
 FILES=( ajax list local manager relation route )
-curl -s -o app/assets/javascripts/spine.coffee https://raw.github.com/spine/spine/$VERSION/src/spine.coffee
+curl -s -o vendor/assets/javascripts/spine.coffee https://raw.github.com/spine/spine/$VERSION/src/spine.coffee
 
 for i in "${FILES[@]}"
 do
    :
-   curl -s -o app/assets/javascripts/spine/$i.coffee https://raw.github.com/spine/spine/$VERSION/src/$i.coffee
+   curl -s -o vendor/assets/javascripts/spine/$i.coffee https://raw.github.com/spine/spine/$VERSION/src/$i.coffee
 done
