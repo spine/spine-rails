d0ec3f04c818cb4b18069854934afb67d4000b50
Ingolfur Edvardsson <ingolfured@gmail.com>
2015-07-29 17:00:47 +0100

    spine updated, update.sh can now be ran from anywhere

--- build/update.sh
+++ build/update.sh
@@ -1,7 +1,9 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT="$(dirname "$DIR")"
 FILES=( ajax list local manager relation route )
-curl -s -o vendor/assets/javascripts/spine.coffee https://raw.githubusercontent.com/spine/spine/dev/src/spine.coffee
+curl -s -o $ROOT/vendor/assets/javascripts/spine.coffee https://raw.githubusercontent.com/spine/spine/dev/src/spine.coffee
 
 for i in "${FILES[@]}"
 do
-   curl -s  -o vendor/assets/javascripts/spine/$i.coffee https://raw.githubusercontent.com/spine/spine/dev/src/$i.coffee
+   curl -s  -o $ROOT/vendor/assets/javascripts/spine/$i.coffee https://raw.githubusercontent.com/spine/spine/dev/src/$i.coffee
 done
--- lib/spine/rails/version.rb
+++ lib/spine/rails/version.rb
@@ -1,6 +1,6 @@
 module Spine
   module Rails
-    VERSION = "0.1.41"
-    SPINE_VERSION = "1.4.1"
+    VERSION = "0.1.5"
+    SPINE_VERSION = "1.5.0"
   end
 end
